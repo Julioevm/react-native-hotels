cc55d180ad1459ed4088c4b4cd800ddb8907f3e2
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-12-05 23:13:45 +0100

    Tweak header style.

--- App.tsx
+++ App.tsx
@@ -21,9 +21,25 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+        }}>
         <Stack.Screen name="Hotels" component={ListScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={({route}) => ({
+            title: route.params.hotel.name,
+            headerTitleStyle: {
+              fontSize: 16,
+              fontWeight: 'bold',
+            },
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
