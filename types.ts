06ea4ed5c3f60b83b06d1ce3eb9c8763fe3ac8f3
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-12-05 13:21:10 +0100

    Add types for the APi response.

---
+++ types.ts
@@ -0,0 +1,30 @@
+interface Location {
+  address: string;
+  city: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface TimeSpan {
+  from: string;
+  to: string;
+}
+
+interface Contact {
+  phoneNumber: string;
+  email: string;
+}
+
+export interface Hotel {
+  id: string;
+  name: string;
+  location: Location;
+  stars: number;
+  checkIn: TimeSpan;
+  checkOut: TimeSpan;
+  contact: Contact;
+  gallery: string[];
+  userRating: number;
+  price: number;
+  currency: string;
+}
