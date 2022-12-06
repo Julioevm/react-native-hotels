09999dd48c67913f8bbcf2a319a4ac4e4eabcec5
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-12-06 17:45:55 +0100

    Add additional details to DetailsScreen

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,10 +1,11 @@
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {Button, Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {RouteProp} from '@react-navigation/native';
 import {RootStackParamList} from '../types';
 import StarRating from '../components/StarRating';
 import UserRating from '../components/UserRating';
 import Carrousel from '../components/Carrousel';
+import {getCurrencySymbol} from '../utils/i18n';
 
 type Props = {
   route: RouteProp<RootStackParamList, 'Details'>;
@@ -13,15 +14,51 @@ type Props = {
 const SPACING = 16;
 
 export default function DetailsScreen({route}: Props) {
-  const {name, stars, userRating, gallery} = route.params.hotel;
+  const {
+    name,
+    stars,
+    userRating,
+    location,
+    gallery,
+    contact,
+    checkIn,
+    checkOut,
+    price,
+    currency,
+  } = route.params.hotel;
   const width = Dimensions.get('window').width - SPACING * 2;
+  const symbol = getCurrencySymbol(currency);
   return (
     <View style={styles.container}>
       <Carrousel gallery={gallery} width={width} autoPlay={true} />
       <Text style={styles.title}>{name}</Text>
-      <View style={styles.ratings}>
+      <View style={styles.middleWrapper}>
         <StarRating stars={stars} />
         <UserRating rating={userRating} />
+        <View>
+          <Text style={styles.price}>
+            {price} {symbol}
+          </Text>
+        </View>
+      </View>
+      <Text style={styles.underline}>
+        {location.address}, {location.city}
+      </Text>
+      <View style={styles.section}>
+        <Text style={styles.subTitle}>Contact:</Text>
+        <Text>Phone: {contact.phoneNumber}</Text>
+        <Text>Email: {contact.email}</Text>
+        <Text style={styles.subTitle}>Check in:</Text>
+        <Text>
+          From {checkIn.from} to {checkIn.to}
+        </Text>
+        <Text style={styles.subTitle}>Check out: </Text>
+        <Text>
+          From {checkOut.from} to {checkOut.to}
+        </Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title={'Book Now'} color={'tomato'} />
       </View>
     </View>
   );
@@ -31,14 +68,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: SPACING,
+    backgroundColor: 'white',
   },
-  ratings: {
+  section: {
+    borderRadius: 8,
+    backgroundColor: 'lavenderblush',
+    padding: SPACING / 2,
+    marginTop: SPACING / 2,
+  },
+  middleWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: SPACING / 2,
   },
   title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 8,
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subTitle: {
     fontSize: 16,
     fontWeight: 'bold',
-    marginBottom: 8,
+  },
+  buttonWrapper: {
+    marginTop: SPACING,
+  },
+  underline: {
+    textDecorationLine: 'underline',
   },
 });
