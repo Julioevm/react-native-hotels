6b05f5dda21f740cd5421cfd15f876e11cea7451
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-12-07 16:24:05 +0100

    Add theme file.

---
+++ Theme.ts
@@ -0,0 +1,14 @@
+export const Theme = {
+  colors: {
+    primary: 'tomato',
+    secondary: 'lightcoral',
+    background: 'lavenderblush',
+    white: 'white',
+    black: 'black',
+  },
+  sizes: {
+    small: 8,
+    medium: 12,
+    large: 16,
+  },
+};
