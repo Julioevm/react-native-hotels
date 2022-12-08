79ad1acf32d8efbd03eb22d7b5ee1985a5694855
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-12-08 22:56:50 +0100

    Test static filters.

--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -7,9 +7,34 @@ import {Theme} from '../Theme';
 
 const ENDPOINT = 'https://run.mocky.io/v3/eef3c24d-5bfd-4881-9af7-0b404ce09507';
 
+type SortOrder = 'ascending' | 'descending';
+
+type OrderCategory = 'stars' | 'rating' | 'price';
+
+interface Filters {
+  name: string;
+  sort: OrderCategory;
+  order: SortOrder;
+  price: Array<number>;
+}
+
+function sortOrder(order: SortOrder) {
+  if (order === 'ascending') {
+    return 1;
+  } else {
+    return -1;
+  }
+}
 export default function ListScreen() {
   const [data, setData] = useState<Array<Hotel> | undefined>(undefined);
+  const defaultFilter: Filters = {
+    name: '',
+    sort: 'price',
+    order: 'descending',
+    price: [0, 500],
+  };
 
+  const [filters, setFilters] = useState<Filters>(defaultFilter);
   const fetchData = async () => {
     try {
       const response = await fetch(ENDPOINT);
@@ -19,15 +44,38 @@ export default function ListScreen() {
       console.error(error);
     }
   };
+  console.log(filters);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const filteredData =
+    data
+      ?.filter(item =>
+        item.name.toLowerCase().includes(filters.name.toLowerCase()),
+      )
+      .filter(
+        item =>
+          item.price >= filters.price[0] && item.price <= filters.price[1],
+      ) || ([] as Array<Hotel>);
+
+  filteredData.sort((a, b) => {
+    if (filters.sort === 'price') {
+      return sortOrder(filters.order) * (a.price - b.price);
+    } else if (filters.sort === 'rating') {
+      return sortOrder(filters.order) * (a.userRating - b.userRating);
+    } else if (filters.sort === 'stars') {
+      return sortOrder(filters.order) * (a.stars - b.stars);
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <View style={styles.container}>
       <Header />
-      <List data={data} />
+      <List data={filteredData} />
     </View>
   );
 }
